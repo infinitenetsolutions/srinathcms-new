8652fade5487dd419c2d787c756055dfc978784f
Rohit-kumar-raja <72311596+Rohit-kumar-raja@users.noreply.github.com>
2021-09-05 00:04:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -140,7 +140,7 @@ if (isset($_POST['done'])) {
                                    
                                     <p class="errormsg col-sm-6"><?php echo $smg; ?></p></div>
                                     <?php echo $otp; ?>
-                          
+<!--                           here i have removed br tag -->
                                     <div class=" ad group col-sm-6"> <input required type="submit" class=" button btn-primary" name="submit" value="Get Otp"> </div>
                                   
                                     <div class="hr"></div>

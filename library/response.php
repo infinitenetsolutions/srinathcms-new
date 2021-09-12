deb15992327cbb28e43e7ddbc5d6630cdf97d6ba
raja <raja@raja.com>
2021-09-12 15:46:44 +0530

    done

--- library/response.php
+++ library/response.php
@@ -1,7 +1,7 @@
 <?php
 // include file
 include_once('easebuzz-lib/easebuzz_payment_gateway.php');
-include '../Backend/connection.inc.php';
+// include '../Backend/connection.inc.php';
 // salt for testing env
 $SALT = "92HHB0OBFH";
 
@@ -31,7 +31,8 @@ $result = $easebuzzObj->easebuzzResponse($_POST);
 
 // print_r($result);
 $result = json_decode($result, true);
-
+session_start();
+$connection=mysqli_connect("localhost","phpmyadmin","raja@#","srinath_cms");
 //echo "<h1>EaseBuzz</h1>"; 
 // $chk_tx = $result["data"]["status"];
 // echo "<br>";
@@ -50,7 +51,7 @@ $easebuzz_id=$result['data']['easepayid'];
 $transaction_id=$result['data']['txnid'];
 $status=md5('visible');
 $email=$result['data']['email'];
-
+$_SESSION['email']=$email;
 echo $update_payment="UPDATE `tbl_prospectus` SET `payment_status`='$payment_status',`prospectus_rate`='$prospectus_rate',`prospectus_payment_mode`='$prospectus_payment_mode',`prospectus_deposit_to`='$prospectus_deposit_to',`bank_name`='$bank_name',`transaction_no`='$transaction_no',`transaction_date`='$transaction_date',`post_at`='NULL',`type`='$type',`easebuzz_id`='$easebuzz_id',`transaction_id`='$transaction_id',`status`='$status' WHERE `prospectus_emailid`='$email' ";
 $update_payment_result=mysqli_query($connection,$update_payment);
 if($update_payment_result){

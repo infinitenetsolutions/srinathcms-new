ece9c01b579464000c3f7c026a9afd8d5361cae0
raja <raja@raja.com>
2021-09-12 14:03:34 +0530

    done update

--- library/response.php
+++ library/response.php
@@ -29,14 +29,33 @@ $easebuzzObj = new Easebuzz($MERCHANT_KEY = null, $SALT, $ENV = null);
 
 $result = $easebuzzObj->easebuzzResponse($_POST);
 
-print_r($result);
+// print_r($result);
 $result = json_decode($result, true);
 
 //echo "<h1>EaseBuzz</h1>"; 
-$chk_tx = $result["data"]["status"];
-$bank_ref_num=['data']['bank_ref_num'];
-$bank_ref_num=['data']['bank_ref_num'];
-$bank_ref_num=['data']['bank_ref_num'];
-$bank_ref_num=['data']['bank_ref_num'];
-$bank_ref_num=['data']['bank_ref_num'];
+// $chk_tx = $result["data"]["status"];
+// echo "<br>";
+$payment_status=$result['data']['status'];
+if($payment_status=='success'){
 
+$prospectus_rate=$result['data']['net_amount_debit'];
+$prospectus_payment_mode=$result['data']['mode'];
+$prospectus_deposit_to=$result['data']['bank_ref_num'];
+$bank_name=$result['data']['bank_name'];
+$transaction_no=$result['data']['txnid'];
+$transaction_date=$result['data']['addedon'];
+// $post_at=$result['data']['bank_ref_num'];
+$type=$result['data']['PG_TYPE'];
+$easebuzz_id=$result['data']['easepayid'];
+$transaction_id=$result['data']['txnid'];
+$status=md5('visible');
+$email=$_SESSION['email'];
+$update_payment="UPDATE `tbl_prospectus` SET `payment_status`='$payment_status',`prospectus_rate`='$prospectus_rate',`prospectus_payment_mode`='$prospectus_payment_mode',`prospectus_deposit_to`='$prospectus_deposit_to',`bank_name`='$bank_name',`transaction_no`='$transaction_no',`transaction_date`='$transaction_date',`post_at`='NULL',`type`='$type',`easebuzz_id`='$easebuzz_id',`transaction_id`='$transaction_id',`status`='$status' WHERE `prospectus_emailid`='$email' ";
+$update_payment_result($connection,$update_payment);
+if($update_payment_result){
+echo '<script> window.location.replace("../print.php") </script>';
+}
+}
+else{
+    echo '<script> window.location.replace("../registration.php") </script>';
+}

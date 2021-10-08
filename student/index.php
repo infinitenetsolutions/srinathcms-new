e6880c89e54c5e5182bf8140ba4311fe7e3e615a
raja <raja@raja.com>
2021-10-08 11:05:02 +0530

    done

--- student/index.php
+++ student/index.php
@@ -1,146 +1,347 @@
 <?php
-if (empty(session_start()))
-    session_start();
-//DataBase Connectivity
-include "include/config.php";
-if (isset($_SESSION["logger_username1"]) && isset($_SESSION["logger_password1"])) {
-    echo "<script> location.replace('dashboard'); </script>";
-} else {
+
+//attendance.php
+
+include('header.php');
+
 ?>
 
-    <body>
-        <title> SRINATH UNIVERSITY LOGIN PAGE</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <link href="dist/css/login.css" rel="stylesheet" id="bootstrap-css">
-
-        <style type="text/css">
-            body {
-                background: url('images/img_bg_nsu.png') fixed;
-                background-repeat: no-repeat;
-                background-size: cover;
-                padding: 0;
-                margin: 0;
-
-            }
-
-            .img-title {
-                margin-top: 30%;
-            }
-
-            .title {
-                color: red;
-                font-size: 30px;
-                font-weight: 900;
-                margin-top: 10%;
-
-            }
-
-            .p-white {
-                color: white;
-            }
-
-            @media (min-width: 768px) {
-                .margin {
-                    margin-left: -40px;
-                }
-            }
-        </style>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-5">
-                    <div class="wrap">
-
-
-                        <center class="margin"> <img class="img-title" src="images/images.png">
-
-
-                            <p class="title">STUDENT LOGIN</p>
-                            <form method="POST" id="student_login_form">
-                                <div id="error_section"></div>
-                                <b class="p-white"> User ID :</b><input type="text" id="student_login_username" name="student_login_username" class="form-control" placeholder="Username"></br>
-                                <b class="p-white">Password : </b><input type="password" id="student_login_password" name="student_login_password" class="form-control" placeholder="Password"></br>
-                                <div class="col-4">
-                                    <div class="col-4">
-                                        <input type='hidden' name='action' value='student_login' />
-                                        <button type="submit" id="student_login_button" name="student_login_button" class="btn btn-primary btn-block">Sign In</button>
-                                    </div>
-                                    <div class="col-12" id="loader_section"></div>
-                                </div>
-                            </form>
-                        </center>
-                        <br><br>
-                        <p class="p-white"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; If you don't have your Login User Id and Password <a href="http://65.2.38.42/" style="color:red"> Click </a></p>
-                        <br>
-                        <br>
-                    </div>
-                </div>
-                <div class="col-sm-3">
-
-                </div>
-            </div>
-
+<div class="container" style="margin-top:30px">
+  <div class="card">
+  	<div class="card-header">
+      <div class="row">
+        <div class="col-md-9">Attendance List</div>
+        <div class="col-md-3" align="right">
+          <button type="button" id="report_button" class="btn btn-danger btn-sm">Attendance Report</button>
+          <button type="button" id="add_button" class="btn btn-info btn-sm">Add</button>
         </div>
-        <script>
-            $(function() {
+      </div>
+    </div>
+    <div class="card-header">
+      <div class="row">
+        <div class="col-md-6">All Student Attendance List</div>
+        <div class="col-md-6" align="right">
+        	<select style="width:100%;" class="form-control" onchange="changeGrade()" id="changeGradeId" >
+        	    <option value="all" <?php if($row["grade_id"] == "all"){ echo "disabled selected"; } ?> >All</option>
+        	    <?php 
+        	        $query = "
+            		SELECT * FROM tbl_grade ORDER BY grade_name ASC
+            		";
+            		$statement = $connect->prepare($query);
+            		$statement->execute();
+            		$result = $statement->fetchAll();
+            		foreach($result as $row){
+            	?>
+        	            <option value="<?php echo $row["grade_id"]; ?>" <?php if($row["grade_id"] == $_GET["grade"]){ echo "disabled selected"; } ?> ><?php echo $row["grade_name"]; ?></option>
+        	    <?php } ?>
+        	    
+        	</select>
+        </div>
+      </div>
+    </div>
+  	<div class="card-body">
+  		<div class="table-responsive">
+        <span id="message_operation"></span>
+        <table class="table table-striped table-bordered" id="attendance_table">
+          <thead>
+            <tr>
+              <th>Student Name</th>
+              <th>Roll Number</th>
+              <th>Grade</th>
+              <th>Attendance Status</th>
+              <th>Attendance Date</th>
+            </tr>
+          </thead>
+          <tbody>
 
-                $('#student_login_form').submit(function(event) {
-                    $('#loader_section').append('<center id = "loading"><img width="50px" src = "images/load.gif" alt="Currently loading" /></center>');
-                    $('#student_login_button').prop('disabled', true);
-                    $.ajax({
-                        url: 'include/controller.php',
-                        type: 'POST',
-                        data: $('#student_login_form').serializeArray(),
-                        success: function(result) {
-                            $('#response').remove();
-                            $('#student_login_form')[0].reset();
-                            $('#error_section').append('<div id = "response">' + result + '</div>');
-                            $('#loading').fadeOut(500, function() {
-                                $(this).remove();
-                            });
-                            $('#student_login_button').prop('disabled', false);
-                        }
+          </tbody>
+        </table>
+  		</div>
+  	</div>
+  </div>
+</div>
 
-                    });
-                    event.preventDefault();
-                });
+</body>
+</html>
 
-            });
-        </script>
+<script type="text/javascript" src="../js/bootstrap-datepicker.js"></script>
+<link rel="stylesheet" href="css/datepicker.css" />
 
-    </body>
-    <style>
-        .or {
-            color: orangered !important;
+<style>
+    .datepicker
+    {
+      z-index: 1600 !important; /* has to be larger than 1050 */
+    }
+</style>
+
+<?php
+$grade_id = 0;
+if($_GET["grade"] != "all" && !empty($_GET["grade"]))
+    $grade_id = $_GET["grade"];
+$query = "
+SELECT * FROM tbl_grade WHERE grade_id = $grade_id
+";
+
+$statement = $connect->prepare($query);
+$statement->execute();
+$result = $statement->fetchAll();
+
+?>
+
+<div class="modal" id="formModal">
+  <div class="modal-dialog">
+    <form method="post" id="attendance_form">
+      <div class="modal-content">
+
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title" id="modal_title"></h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+
+        <!-- Modal body -->
+        <div class="modal-body">
+            <?php if($grade_id == "0"){ ?>
+                <center><span style="color:red;"> ...Please first select Grade...</span></center>
+            <?php } ?>
+          <?php
+          foreach($result as $row)
+          {
+          ?>
+          <div class="form-group">
+            <div class="row">
+              <label class="col-md-4 text-right">Grade <span class="text-danger">*</span></label>
+              <div class="col-md-8">
+                <?php
+                echo '<label>'.$row["grade_name"].'</label>';
+                ?>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <label class="col-md-4 text-right">Attendance Date <span class="text-danger">*</span></label>
+              <div class="col-md-8">
+                <input type="text" name="attendance_date" id="attendance_date" class="form-control" readonly />
+                <span id="error_attendance_date" class="text-danger"></span>
+              </div>
+            </div>
+          </div>
+          <div class="form-group" id="student_details">
+            <div align="right">
+                <a class="btn btn-success text-white" onclick="location.replace('?grade=<?php echo $_GET["grade"]; ?>&option=present')" >Present All</a>
+                <a class="btn btn-danger text-white" onclick="location.replace('?grade=<?php echo $_GET["grade"]; ?>&option=absent')" >Absent All</a>
+            </div>
+            <br/>
+            <div class="table-responsive">
+              <table class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th>Roll No.</th>
+                    <th>Student Name</th>
+                    <th>Present</th>
+                    <th>Absent</th>
+                  </tr>
+                </thead>
+                <?php
+                $sub_query = "
+                  SELECT * FROM tbl_student 
+                  WHERE student_grade_id = '".$row["grade_id"]."'
+                ";
+                $statement = $connect->prepare($sub_query);
+                $statement->execute();
+                $student_result = $statement->fetchAll();
+                foreach($student_result as $student)
+                {
+                ?>
+                  <tr>
+                    <td><?php echo $student["student_roll_number"]; ?></td>
+                    <td>
+                      <?php echo $student["student_name"]; ?>
+                      <input type="hidden" name="student_id[]" value="<?php echo $student["student_id"]; ?>" />
+                    </td>
+                    <td>
+                      <input type="radio" name="attendance_status<?php echo $student["student_id"]; ?>" <?php if(!isset($_GET["option"]) || $_GET["option"] == "present"){ echo "checked"; } ?> value="Present" />
+                    </td>
+                    <td>
+                      <input type="radio" name="attendance_status<?php echo $student["student_id"]; ?>" <?php if(isset($_GET["option"]) && $_GET["option"] == "absent"){ echo "checked"; } ?> value="Absent" />
+                    </td>
+                  </tr>
+                <?php
+                }
+                ?>
+              </table>
+            </div>
+          </div>
+          <?php
+          }
+          ?>
+        </div>
+
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <input type="hidden" name="action" id="action" value="Add" />
+          <input type="submit" name="button_action" id="button_action" class="btn btn-success btn-sm" value="Add" />
+          <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
+        </div>
+
+      </div>
+    </form>
+  </div>
+</div>
+
+<div class="modal" id="reportModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Make Report</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+     <form method="POST" action="export-excel.php">
+          <!-- Modal body -->
+          <div class="modal-body">
+            <div class="form-group">
+              <div class="input-daterange">
+                <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date"  required autocomplete="off" />
+                <span id="error_from_date" class="text-danger"></span>
+                <br />
+                <input type="text" name="to_date" id="to_date" class="form-control" placeholder="To Date"  required autocomplete="off" />
+                <span id="error_to_date" class="text-danger"></span>
+              </div>
+            </div>
+          </div>
+          <!-- Modal footer -->
+          <div class="modal-footer">
+            <input type="hidden" name="withGrade" value="<?php if(!isset($_GET["grade"])) echo "all"; else echo $_GET["grade"]; ?>" />
+            <button type="submit" name="create_report" id="create_excel_report" class="btn btn-success btn-sm">Create Report</button>
+            <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
+          </div>
+    </form>
+    </div>
+  </div>
+</div>
+<?php include '../include/footer.php'; ?>
+<script>
+    function changeGrade(){
+        var changeValue = document.getElementById('changeGradeId').value;
+        window.location.href = "add_attendance.php?grade="+changeValue;
+    }
+</script>
+<script>
+$(document).ready(function(){
+	
+  var dataTable = $('#attendance_table').DataTable({
+    "processing":true,
+    "serverSide":true,
+    "order":[],
+    "ajax":{
+      url:"add_attendance_action.php<?php if(empty($_GET["grade"])){ echo ""; } else if($_GET["grade"]=="all"){ echo ""; } else{ echo "?grade=".$_GET["grade"]; } ?>",
+      method:"POST",
+      data:{action:"fetch"}
+    }
+  });
+
+  $('#attendance_date').datepicker({
+    format:'yyyy-mm-dd',
+    autoclose:true,
+    container: '#formModal modal-body'
+  });
+
+  function clear_field()
+  {
+    $('#attendance_form')[0].reset();
+    $('#error_attendance_date').text('');
+  }
+
+  $('#add_button').click(function(){
+    $('#modal_title').text("Add Attendance");
+    $('#formModal').modal('show');
+    clear_field();
+  });
+
+  $('#attendance_form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url:"add_attendance_action.php<?php if(empty($_GET["grade"])){ echo ""; } else if($_GET["grade"]=="all"){ echo ""; } else{ echo "?grade=".$_GET["grade"]; } ?>",
+      method:"POST",
+      data:$(this).serialize(),
+      dataType:"json",
+      beforeSend:function(){
+        $('#button_action').val('Validate...');
+        $('#button_action').attr('disabled', 'disabled');
+      },
+      success:function(data)
+      {
+        $('#button_action').attr('disabled', false);
+        $('#button_action').val($('#action').val());
+        if(data.success)
+        {
+          $('#message_operation').html('<div class="alert alert-success">'+data.success+'</div>');
+          clear_field();
+          $('#formModal').modal('hide');
+          dataTable.ajax.reload();
         }
-
-        .text-center {
-
-            color: white;
-
-            margin-top: 110rem;
+        if(data.error)
+        {
+          if(data.error_attendance_date != '')
+          {
+            $('#error_attendance_date').text(data.error_attendance_date);
+          }
+          else
+          {
+            $('#error_attendance_date').text('');
+          }
         }
+      }
+    })
+  });
 
-        @media screen and (min-width: 560px) {
-            .text-center {
+  $('.input-daterange').datepicker({
+    todayBtn:"linked",
+    format:"yyyy-mm-dd",
+    autoclose:true,
+    container: '#formModal modal-body'
+  });
 
-                color: white;
+  $(document).on('click', '#report_button', function(){
+    $('#reportModal').modal('show');
+  });
 
-                margin-top: 63rem;
-            }
-        }
-    </style>
+  $('#create_report').click(function(){
+    var from_date = $('#from_date').val();
+    var to_date = $('#to_date').val();
+    var error = 0;
+    if(from_date == '')
+    {
+      $('#error_from_date').text('From Date is Required');
+      error++;
+    }
+    else
+    {
+      $('#error_from_date').text('');
+    }
 
-<footer class=" footer text-center mt-5 p-5">
+    if(to_date == '')
+    {
+      $('#error_to_date').text("To Date is Required");
+      error++;
+    }
+    else
+    {
+      $('#error_to_date').text('');
+    }
 
-<strong><p>&copy; <?php echo date("Y"); ?> <a class="or" style="color: #a82b12;" target="_blank" href="#">SRINATH UNIVERSITY </a> All Right Reserved. Powered By  <a class="text-success" href="#" target="_blank">Infinite Net Solutions</a></p>
-</footer>
+    if(error == 0)
+    {
+      $('#from_date').val('');
+      $('#to_date').val('');
+      $('#formModal').modal('hide');
+      window.open("report.php?action=attendance_report_teacher&from_date="+from_date+"&to_date="+to_date);
+    }
 
-    </html>
-<?php } ?>
-<!--  rtrt-->
+  });
+
+});
+</script>

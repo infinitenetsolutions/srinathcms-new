77ddb098ea5c5793b64052f3886eac0ec653e2ab
Rohit-kumar-raja <72311596+Rohit-kumar-raja@users.noreply.github.com>
2021-10-29 18:46:36 +0530

    addf

--- srinath/dashboard.php
+++ srinath/dashboard.php
@@ -419,7 +419,7 @@ expense.push('" . $total_expense_amount . "')
 }
 else{
   
-  echo  $total_income = "SELECT SUM(amount) as total FROM `tbl_income` WHERE post_at BETWEEN '$year-$i-01' AND '$year-0$next-01' ";
+  echo  $total_income = "SELECT SUM(amount) as total FROM `tbl_income` WHERE post_at BETWEEN '$year-$i-01' AND '$year-$next-01' ";
     $income_result = mysqli_query($con, $total_income);
     $total_income_data = mysqli_fetch_array($income_result);
     $total_income_amount = $total_income_data['total'];
@@ -429,7 +429,7 @@ else{
 income.push('" . $total_income_amount . "')
 </script>";
 
-    $total_expense = "SELECT SUM(amount) as total FROM `tbl_expenses` WHERE payment_date BETWEEN '$year-$i-01' AND '$year-0$next-01' ";
+    $total_expense = "SELECT SUM(amount) as total FROM `tbl_expenses` WHERE payment_date BETWEEN '$year-$i-01' AND '$year-$next-01' ";
     $expense_result = mysqli_query($con, $total_expense);
     $total_expense_data = mysqli_fetch_array($expense_result);
     $total_expense_amount = $total_expense_data['total'];

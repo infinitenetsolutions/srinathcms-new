d25e59f9b8b1f8e3baee56d09ed60a25385ed0ae
raja <raja@raja.com>
2021-11-17 10:54:57 +0530

    sone

--- srinath/include/config.php
+++ srinath/include/config.php
@@ -2,10 +2,10 @@
     // Create connection
     //$con = new mysqli("localhost", "", "", "");
     if($_SERVER['HTTP_HOST']=='localhost'){
-        $con = new mysqli('localhost','root','','srinath_cms');
+        $con = mysqli_connect('localhost','root','','srinath_cms');
     }
     else{
-    $con = new mysqli("localhost", "root", "raja@#", "srinath_cms");
+    $con = mysqli_connect("localhost", "root", "raja@#", "srinath_cms");
     // Check connection
     if ($con->connect_error) {
         die("Connection failed: " . $con->connect_error);

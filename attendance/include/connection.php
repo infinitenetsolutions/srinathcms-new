6a768ed4434860fa2a8d5097d2e0d176799dec6a
raja <raja@raja.com>
2021-11-30 16:07:17 +0530

    done

--- attendance/include/connection.php
+++ attendance/include/connection.php
@@ -4,6 +4,6 @@ if($_SERVER['HTTP_HOST']=="localhost"){
 	$connection = 	mysqli_connect("localhost","root","" ,"srinath_atteandance");
 }
 else{
-    $connection = 	mysqli_connect("localhost","phpmyadmin","Raja@#","srinath_atteandance");  
+    $connection = 	mysqli_connect("localhost","phpmyadmin","raja@#","srinath_atteandance");  
 }
 ?>

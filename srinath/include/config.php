681a5f5ed53e68a4d21dc2bbe0a03f1ecc337540
raja <raja@raja.com>
2022-01-11 15:46:24 +0530

    all done

--- srinath/include/config.php
+++ srinath/include/config.php
@@ -12,7 +12,7 @@ if ($_SERVER['HTTP_HOST'] == 'localhost') {
     if ($con->connect_error) {
         die("Connection failed: " . $con->connect_error);
     }else{
-        echo "connection success"
+        echo "connection success";
     }
 }
 
